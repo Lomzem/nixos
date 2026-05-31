67e8e7d73c2259dd5bc7943bca61a49cb18c7833
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-31 00:20:02 -0700

    feat(home): manage git config

--- modules/home/users/lomzem/git.nix
+++ modules/home/users/lomzem/git.nix
@@ -1,7 +1,39 @@
+{ pkgs, ... }:
+
 {
   programs.git = {
     enable = true;
     settings = {
+      alias = {
+        a = "add";
+        amend = "commit --amend";
+        b = "branch";
+        c = "clone";
+        co = "checkout";
+        d = "diff --cached";
+        dc = "diff --cached";
+        df = "diff";
+        hist = "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short";
+        last = "log -1 HEAD";
+        p = "pull";
+        ph = "push";
+        r = "restore";
+        show = "show --ext-diff";
+        s = "status";
+        u = "reset HEAD --";
+        unstage = "reset HEAD --";
+      };
+      color.ui = "auto";
+      core.pager = "bat";
+      credential.helper = "store";
+      diff = {
+        external = "difft";
+        tool = "difftastic";
+      };
+      push = {
+        autoSetupRemote = true;
+        default = "current";
+      };
       user = {
         name = "Lawjay Lee";
         email = "lawjaylee1@icloud.com";
@@ -10,4 +42,9 @@
       pull.rebase = true;
     };
   };
+
+  home.packages = with pkgs; [
+    bat
+    difftastic
+  ];
 }
