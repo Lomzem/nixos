670f366327b4537c88b7d86c5979a1b496b8e87f
Lawjay Lee <lawjaylee1@icloud.com>
2026-05-30 23:58:00 -0700

    fix(nvim): source plugins from config path

--- modules/home/users/lomzem/neovim-config/init.lua
+++ modules/home/users/lomzem/neovim-config/init.lua
@@ -20,7 +20,7 @@ vim.keymap.set("n", "<a-e>", "<cmd>NvimTreeToggle<cr>")
 vim.pack.add({ "https://github.com/mrcjkb/rustaceanvim" })
 
 -- Source all in `lua/plugins`
-local plugins_dir = vim.fs.joinpath(vim.api.nvim_get_runtime_file("lua/plugins", false)[1])
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath("config"), "lua", "plugins")
 for name, type in vim.fs.dir(plugins_dir, { depth = math.huge }) do
 	if type == "file" then
 		local abspath = vim.fs.joinpath(plugins_dir, name)
